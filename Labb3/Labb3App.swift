a6d8dcc1b5fd25ee3e62f0dac0e8fd4ca5af2873
Bashar <abosalim2003@gmail.com>
2026-03-08 20:53:24 +0100

    Initial Commit

---
+++ Labb3/Labb3App.swift
@@ -0,0 +1,17 @@
+//
+//  Labb3App.swift
+//  Labb3
+//
+//  Created by Bashar Abdulsalam on 2026-03-08.
+//
+
+import SwiftUI
+
+@main
+struct Labb3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
