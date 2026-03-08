914e3344e50bcd687efff72e3e1108ea0fde7f22
Bashar <abosalim2003@gmail.com>
2026-03-09 00:27:03 +0100

    variables

--- Labb3/ContentView.swift
+++ Labb3/ContentView.swift
@@ -1,10 +1,13 @@
 import SwiftUI
 
-
 struct TriviaQuestion: Codable {
     let question: String
     let correct_answer: String
     let incorrect_answers: [String]
+
+    var allAnswers: [String] {
+        (incorrect_answers + [correct_answer]).shuffled()
+    }
 }
 
 struct TriviaResponse: Codable {
@@ -12,6 +15,12 @@ struct TriviaResponse: Codable {
 }
 
 struct ContentView: View {
+    @State private var questions: [TriviaQuestion] = []
+    @State private var currentIndex = 0
+    @State private var score = 0
+    @State private var isFinished = false
+    @State private var isLoading = false
+
     var body: some View {
         Text("Pop Quiz")
             .font(.largeTitle)
